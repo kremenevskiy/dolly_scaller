93da5cee058c16cda1e7079bfd266bbd2943a090
kremenevskiy <kremeneuski@gmial.com>
2025-02-02 06:54:01 +0000

    drop no need core

--- src/config.py
+++ src/config.py
@@ -1,13 +1,10 @@
 import os
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any
 
 from dotenv import load_dotenv
-from pydantic import PostgresDsn, model_validator
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings
 
-from src.constants import Environment
 from src.logger import logger
 
 
@@ -41,40 +38,12 @@ class DatabaseConf:
     db_port: int = int(os.environ['DB_PORT'])
 
 
-class CustomBaseSettings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra='ignore')
-
-
-class Config(CustomBaseSettings):
-    DATABASE_URL: PostgresDsn
-    DATABASE_ASYNC_URL: PostgresDsn
-    DATABASE_POOL_SIZE: int = 16
-    DATABASE_POOL_TTL: int = 60 * 20  # 20 minutes
-    DATABASE_POOL_PRE_PING: bool = True
-
-    ENVIRONMENT: Environment = Environment.PRODUCTION
-
-    SENTRY_DSN: str | None = None
-
+class Config(BaseSettings):
     CORS_ORIGINS: list[str] = ['*']
     CORS_ORIGINS_REGEX: str | None = None
     CORS_HEADERS: list[str] = ['*']
 
     APP_VERSION: str = '0.1'
 
-    @model_validator(mode='after')
-    def validate_sentry_non_local(self) -> 'Config':
-        if self.ENVIRONMENT.is_deployed and not self.SENTRY_DSN:
-            raise ValueError('Sentry is not set')
-
-        return self
-
 
 settings = Config()
-
-app_configs: dict[str, Any] = {'title': 'App API'}
-if settings.ENVIRONMENT.is_deployed:
-    app_configs['root_path'] = f'/v{settings.APP_VERSION}'
-
-if not settings.ENVIRONMENT.is_debug:
-    app_configs['openapi_url'] = None  # hide docs
--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
 
-import sentry_sdk
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from src import database
-from src.config import app_configs, settings
+from src.config import settings
 from src.model.router import model_router
 from src.subscription_details.router import subscription_router
 from src.user.router import user_router
@@ -16,27 +15,21 @@ from src.user.router import user_router
 async def lifespan(_application: FastAPI) -> AsyncGenerator:
     # Startup
     await database.DatabaseManager.init_pool()
-    print("✅ Database connection pool initialized")
+    print('✅ Database connection pool initialized')
     yield
     # Shutdown
 
 
-app = FastAPI(**app_configs, lifespan=lifespan)
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
     allow_origin_regex=settings.CORS_ORIGINS_REGEX,
     allow_credentials=True,
-    allow_methods=("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"),
+    allow_methods=('GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'),
     allow_headers=settings.CORS_HEADERS,
 )
-
-if settings.ENVIRONMENT.is_deployed:
-    sentry_sdk.init(
-        dsn=settings.SENTRY_DSN,
-        environment=settings.ENVIRONMENT,
-    )
 
 
 app.include_router(user_router)
@@ -44,6 +37,6 @@ app.include_router(model_router)
 app.include_router(subscription_router)
 
 
-@app.get("/healthcheck", include_in_schema=False)
+@app.get('/healthcheck', include_in_schema=False)
 async def healthcheck() -> dict[str, str]:
-    return {"status": "ok"}
+    return {'status': 'ok'}
