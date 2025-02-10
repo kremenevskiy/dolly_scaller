6be73aa4584c4d563ec8757d721ff25d23a1d388
Vladimir Turomsha <zver010810@gmail.com>
2025-02-10 22:24:46 +0300

    fix sql script

--- sql_scripts/add_referral_subscription.sql
+++ sql_scripts/add_referral_subscription.sql
@@ -2,4 +2,4 @@ INSERT INTO subscriptions_details (
     subscription_name, subscription_type, cost_rubles, cost_stars, duration,
     is_active, models_count, generation_photos_count
 ) VALUES
-    ('referral_generations_base_pack', 'referral_generations', 0,   0,   NULL,   FAlSE, 0, 20);
+    ('referral_generations_base_pack', 'referral_generations', 0,   0,   NULL,   FALSE, 0, 20);
